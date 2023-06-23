fabc3c3cfbd1f49cae2518ad02a95b01b4386b13
AIMÊ <aimemartins@hotmail.com>
2023-06-23 11:14:22 -0300

    Requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,29 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+
+
+def exists_word(word, instance: Queue):
+    curr = []
+    resultado = []
+    # iteração sobre cada elemento da fila
+    for f in range(len(instance)):
+        arquivo_pesquisado = instance.search(f)
+        arquivo = arquivo_pesquisado["nome_do_arquivo"]
+        linhas = arquivo_pesquisado["linhas_do_arquivo"]
+        # itera sobre cada linha do arquivo
+        for i, linha in enumerate(linhas):
+            if word.lower() in linha.lower():
+                curr.append({"linha": i + 1})
+
+    if curr:
+        resultado.append(
+            {
+                "palavra": word,
+                "arquivo": arquivo,
+                "ocorrencias": curr,
+            }
+        )
+
+    return resultado
 
 
 def search_by_word(word, instance):

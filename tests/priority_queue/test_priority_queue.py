31041b006d27517e115b993ce0308ff582ebe40b
AIMÊ <aimemartins@hotmail.com>
2023-06-22 21:52:02 -0300

    Requisito 6

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,39 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority_queue = PriorityQueue()
+    assert len(priority_queue) == 0
+
+    priority_queue.enqueue({"qtd_linhas": 9, "nome": "arquivo9.txt"})
+    priority_queue.enqueue({"qtd_linhas": 4, "nome": "arquivo4.txt"})
+    priority_queue.enqueue({"qtd_linhas": 2, "nome": "arquivo2.txt"})
+    priority_queue.enqueue({"qtd_linhas": 5, "nome": "arquivo5.txt"})
+    priority_queue.enqueue({"qtd_linhas": 7, "nome": "arquivo7.txt"})
+    priority_queue.enqueue({"qtd_linhas": 11, "nome": "arquivo11.txt"})
+    priority_queue.enqueue({"qtd_linhas": 3, "nome": "arquivo3.txt"})
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        priority_queue.search(7)
+
+    assert len(priority_queue) == 7
+
+    assert priority_queue.search(1) == {
+        "qtd_linhas": 2,
+        "nome": "arquivo2.txt",
+    }
+    assert priority_queue.search(2) == {
+        "qtd_linhas": 3,
+        "nome": "arquivo3.txt",
+    }
+
+    priority_queue.dequeue() == {"qtd_linhas": 4, "nome": "arquivo4.txt"}
+    priority_queue.dequeue() == {"qtd_linhas": 2, "nome": "arquivo2.txt"}
+    priority_queue.dequeue() == {"qtd_linhas": 3, "nome": "arquivo3.txt"}
+    priority_queue.dequeue() == {"qtd_linhas": 9, "nome": "arquivo9.txt"}
+    priority_queue.dequeue() == {"qtd_linhas": 5, "nome": "arquivo5.txt"}
+    priority_queue.dequeue() == {"qtd_linhas": 7, "nome": "arquivo7.txt"}
+    priority_queue.dequeue() == {"qtd_linhas": 11, "nome": "arquivo11.txt"}
+
+    assert len(priority_queue) == 0
